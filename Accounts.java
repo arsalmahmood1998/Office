8bfa0802feb2307906b114ccaf90d6177166464d
arsalmahmood1998 <68080848+arsalmahmood1998@users.noreply.github.com>
2020-08-24 12:41:08 +0500

    Initial commit

---
+++ Accounts.java
@@ -0,0 +1,20 @@
+class Accounts{
+  String name;
+  String manager;
+  int employeeCount;
+  Accounts(String _manager, int _employeeCount){
+    manager=_manager;
+    employeeCount=_employeeCount;
+  }
+  Accounts(String _name){
+    name=_name;
+  }
+ void details(){
+    System.out.println("Accounts department is managed by "+manager);
+    System.out.println("There are "+employeeCount+ " employees in this Department");
+    System.out.println(name+" works in this Department");
+  }
+  void employeNames(){
+    System.out.println(name+" works in this Department");
+  }
+}
---
+++ Office.java
@@ -0,0 +1,33 @@
+class Office{
+  public static void main (String [] args){
+    System.out.println("Welcome to our Office");
+    System.out.println("--------------------------");
+    Employee adil;
+    Employee ali;
+    Employee umair;
+    Employee hamza;
+    Accounts accounts;
+    Accounts displayAccountsEmployeeNames;
+    HR hr;
+    HR displayHREmployeeNames;
+    adil=new Employee("Adil",22);
+    ali= new Employee("Ali",23);
+    umair=new Employee("Umair",34);
+    hamza=new Employee("Hamza",40);
+    accounts=new Accounts(adil.getName(),5);
+    displayAccountsEmployeeNames=new Accounts(umair.getName());
+    hr=new HR(ali.getName(),4);
+    displayHREmployeeNames= new HR(hamza.getName());
+    adil.details();
+    ali.details();
+    umair.details();
+    hamza.details();
+    System.out.println("--------------------------");
+    System.out.println("Our Office has two Departments");
+    accounts.details();
+    displayAccountsEmployeeNames.employeNames();
+    System.out.println("--------------------------");
+    hr.details();
+    displayHREmployeeNames.employeNames();
+  }
+}
